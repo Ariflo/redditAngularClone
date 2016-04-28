a260fcbaecd5e526b7d8c580cbaa7576a2d05c4e
AriFlo <ariflo@arianflores.com>
2016-04-28 15:59:04 -0700

    finished clean up

--- migrations/20160228225721_postComments.js
+++ migrations/20160228225721_postComments.js
@@ -7,5 +7,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('postComments');
+  return knex.schema.dropTable('post_comments');
 };
