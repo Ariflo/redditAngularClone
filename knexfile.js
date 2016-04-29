3af8d5f67511e0ac59bec4619951c7bca7954f04
AriFlo <ariflo@arianflores.com>
2016-04-29 11:12:03 -0700

    finished clean up

--- knexfile.js
+++ knexfile.js
@@ -34,7 +34,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.HEROKU_POSTGRESQL_AQUA_URL,
     pool: {
       min: 2,
       max: 10
